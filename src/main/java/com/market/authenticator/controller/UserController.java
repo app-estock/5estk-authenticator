333eb13e52d03ffc505851a42cf2938b27f5d842
Akshay Shah <akshayshah.1893@gmail.com>
2022-03-26 23:52:22 +0530

    March 26 2022
    Added cross origin

--- src/main/java/com/market/authenticator/controller/UserController.java
+++ src/main/java/com/market/authenticator/controller/UserController.java
@@ -10,14 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @RestController
+@CrossOrigin(origins = "http://65.2.124.176:4200", maxAge = 3600)
 @RequestMapping("/api/v1.0/market/user")
 public class UserController {
 
